79b51c66d4ad628acbd16c78ed9d7242154e87b3
otmanbal <otman.balhor@hainaut-promsoc.be>
2026-01-08 23:39:26 +0100

    Create __manifest__.py

---
+++ odoo/addons/my_product/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Product Custom Fields', # Only required field
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    'depends': [
+        'product',
+        'sale_management'
+    ],
+    'data': [
+        'views/product_template_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
